4263ea61f592bd1c49946c628316c409fb209a8b
luisa <lufitha1999@gmail.com>
2024-03-18 18:56:42 -0500

    first commit

---
+++ 1-clase.js
@@ -0,0 +1,11 @@
+// comentarios, anotaciones, lineas que se ignoran al ejecutar el programa
+// se usan para guiara la persona que este interpretando el codigo
+
+let variable = 10;
+let sumas = 5;
+
+function suma (uno, dos){
+ resultado = uno + dos
+ return resultado
+}
+suma(variable,sumas)
